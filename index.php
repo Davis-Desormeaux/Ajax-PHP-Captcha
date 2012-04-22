773e438b6dfc6eb302a6813a7e1ae5028de227f0
Davis Desormeaux <davis100@gmail.com>
2012-04-22 07:01:18 -0400

    Small Changes

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
             var _element = element.substring(1);
         
             return eltype != '.'
-                ? eltype != '#'
-                    ? document.getElementsByName(element)
-                    : document.getElementById(_element)
-                : document.getElementsByClassName(_element)
+                    ? eltype != '#'
+                        ? document.getElementsByName(element)
+                        : document.getElementById(_element)
+                    : document.getElementsByClassName(_element);
         }
         
         function dooAjax(url,callback,postQuery) {
@@ -22,16 +22,12 @@
                 return;
             }  
             var method = (postQuery) ? "POST" : "GET";
-            http.open(method,url,true);
-               
+            http.open(method,url,true);               
             if (postQuery) {
                 http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            }               
+            }                           
             http.onreadystatechange = function() {
-                if ((http.readyState != 4) || (http.status != 200 && http.status != 304)) {
-                    return;
-                }            
-                callback(http.responseText);
+                if (http.readyState == 4) callback(http.responseText);        
             }  
             http.send(postQuery);
         }
@@ -39,21 +35,20 @@
         function getXMLHTTPObject() {  
             var xmlHttp = false;
             var AjaxClients = [
-                function () {return new XMLHttpRequest()},
-                function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+                function () {return new ActiveXObject("Microsoft.XMLHTTP")},
                 function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-                function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+                function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+                function () {return new XMLHttpRequest()}
             ];
-        
             
-            for (var i=0; i<AjaxClients.length; i++) {
+            do {
                 try {
-                    xmlHttp = AjaxClients[i]();
+                    xmlHttp = AjaxClients.pop()();
                 } catch (e) {
                     continue;
                 }
-                break;
-            }
+            } while (!xmlHttp && AjaxClients.length != 0)            
+            
             return xmlHttp;
         }          
 
@@ -65,49 +60,54 @@
        
         function validateCaptcha() {
             var usr_captcha = dooSelect('#captchatxt').value;         
-            if (usr_captcha.length < 2) return; // No captcha generated under 2 chars...
+            if (usr_captcha.length < 2) {
+                return; // No captcha generated under 2 chars...       
+            }     
             dooAjax('captchaRestCheck.php?usrcap=' + usr_captcha, function(data){           
                 if(data == "valid") {
                    validCaptcha = 1;
-                   dooSelect('#incorrect').innerHTML = " <b>Valid</b>";
+                   dooSelect('#incorrect').innerHTML    = " <b>Valid</b>";
                    dooSelect('#incorrect').style.color  = "Green";
                 } else {
                    validCaptcha = 0;
-                   dooSelect('#incorrect').innerHTML = "";
+                   dooSelect('#incorrect').innerHTML    = "";
                    dooSelect('#incorrect').style.color  = "red";                
                 }
             });
         }
         
         function doFormSubmit() {
-          if (!validCaptcha) {
-            dooSelect('#incorrect').innerHTML  = "<b> Incorrect</b>";
+          if (!validCaptcha) {            
+            dooSelect('#incorrect').innerHTML    = "<b> Incorrect</b>";
+            dooSelect('#incorrect').style.color  = "red"; 
             dooSelect('#captcha').src = "images/captcha.php?cx=" + Math.random();
             return false;
           } else {
             alert('Great!');
             dooSelect('#incorrect').innerHTML  = "";
             return true;
-          }
-          
+          }          
+        }
+        
+        function put(args){
+            document.write(args);
         }              
     //-->
     </script>
-</html>
+</head>
+<body>
 <h1>Ajax PHP Captcha DEMO</h1>
 <p>
-  <form action="#" method="post" name="captcha-demo" onsubmit="return doFormCheck()">
-    <div id="contact_email">
-      <img src="images/captcha.php?cx=1686237619" id="captcha" class="capt">
+  <form action="#" method="post" name="captcha-demo" onsubmit="return doFormSubmit()">
+      <script>put('<img src="images/captcha.php?cx=' + Math.random() + '" id="captcha">')</script> 
       <br>
-      <a href="#capt" onclick="refreshCapcha()" id="change-image">Refresh</a>:
+      <a href="#capt" onclick="refreshCapcha()" id="change-image">Refresh:</a>
       <label id="incorrect" style="color:red"></label>
       <br>
       CAPTCHA (Validation):
       <input type="text" name="usrcaptcha" id="captchatxt" onkeyup='validateCaptcha()'>
       <br>
       <button class="button validate" type="submit">Send</button>
-    </div>
   </form>
 </p>
 </body>
