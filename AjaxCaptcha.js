02f31ba2db6c94ead8127e5661114b1b80672c06
Davis Desormeaux <davis100@gmail.com>
2012-05-01 06:33:44 -0400

    Made it plain javascript (vanilla).

--- AjaxCaptcha.js
+++ AjaxCaptcha.js
@@ -2,106 +2,131 @@
  *                   GNU GENERAL PUBLIC LICENSE
  *                     Version 3, 29 June 2007
  *
- * Copyright (C) 2012 Davis Desormeaux  
+ * Copyright (C) 2012 Davis Desormeaux
  * More: https://github.com/Davis-Desormeaux
  *
  */
 
-function AjaxCaptcha() {
+var ajxCaptcha = (function() {
+
+  var parameterMap = {
+    txtValid   : 'Valid',
+    validColor : 'Green', 
+    txtBad     : 'Incorrect',
+    badColor   : 'Red'
+  };
+    
+  var validCaptcha = { 
+    value : false 
+  };
+              
+  // Public
+  refreshCapcha = function() {
+    dooSelect('#captcha').src = 'images/captcha.php?cx=' + Math.random();
+    dooSelect('#incorrect').innerHTML = "";
+    validCaptcha.value = false;
+  }
   
-    var validCaptcha = { value: 0 };
-    this.dooSelect   = dooSelect; // Public
-    
-    // Public
-    this.refreshCapcha = function() {
-        dooSelect('#incorrect').innerHTML = "";
-        dooSelect('#captcha').src = 'images/captcha.php?cx=' + Math.random();
-        validCaptcha.value = 0;
-    }
-     
-    // Public
-    this.validateCaptcha = function(textboxId) {
-        var usr_captcha = dooSelect(textboxId).value;         
-        if (usr_captcha.length < 2) {
-            return; // No captcha generated under 2 chars...       
-        }     
-        dooAjax('captchaRestCheck.php?usrcap=' + usr_captcha, function(data){           
-            if(data == "valid") {
-             validCaptcha.value = 1;
-               dooSelect('#incorrect').innerHTML    = " <b>Valid</b>";
-               dooSelect('#incorrect').style.color  = "Green";
-            } else {                           
-               if (validCaptcha.value) dooSelect('#incorrect').innerHTML = ""; // Remove Valid label
-               validCaptcha.value = 0;
-               dooSelect('#incorrect').style.color  = "red";                
-            }
-        });
+  // Public
+  var validateCaptcha = function(textboxId) {
+    var usr_captcha = dooSelect(textboxId).value;
+    if(usr_captcha.length < 2 && !validCaptcha.value) {
+      return; // No captcha generated under 2 characters
     }
     
-    // Public
-    this.doFormSubmit = function() {
-        if (!validCaptcha.value) {            
-            dooSelect('#incorrect').innerHTML    = "<b> Incorrect</b>";
-            dooSelect('#incorrect').style.color  = "red"; 
-            dooSelect('#captcha').src = "images/captcha.php?cx=" + Math.random();
-            return false;
-      } else {
-            alert('Great!');
-            dooSelect('#incorrect').innerHTML  = "";
-            return true;
-      }          
-    }   
+    dooAjax('captchaRestCheck.php?usrcap=' + usr_captcha, function(data) {
+      var outputElem = dooSelect('#incorrect');
+      if(data == "valid") {
+        outputElem.style.color = outputElem.validColor;
+        outputElem.innerHTML = outputElem.txtValid;
+        validCaptcha.value = true;
+      } else {    
+        outputElem.style.color = parameterMap.badColor;
+        validCaptcha.value = false;        
+        if ( validCaptcha.value ) {
+          // Remove the word valid.
+          outputElem.innerHTML = ""; 
+        } 
+      }
+    });
+  }
   
-    // Private
-    function dooAjax (url,callback,postQuery) {
-        var http = getXMLHTTPObject();               
-        if (!http || !url) {
-            console.log(!http ? "Error No XMLHTTP available" : "Error: Empty URL");
-            return;
-        }  
-        var method = (postQuery) ? "POST" : "GET";
-        http.open(method,url,true);               
-        if (postQuery) {
-            http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        }                           
-        http.onreadystatechange = function() {
-            if (http.readyState == 4) callback(http.responseText);        
-        }  
-        http.send(postQuery);
+  // Public
+  doFormSubmit = function() {
+    if(!validCaptcha.value) {
+      dooSelect('#captcha').src = "images/captcha.php?cx=" + Math.random();
+      dooSelect('#incorrect').style.color = parameterMap.badColor;
+      dooSelect('#incorrect').innerHTML = parameterMap.txtBad;
+      return false;      
+    } else {
+      dooSelect('#incorrect').innerHTML = ""; 
+      return true;     
+    }
+  }
+
+  // Private
+  function dooAjax(url, callback, postQuery) {
+    var http = getXMLHTTPObject();
+    
+    if(!http || !url) {
+      console.log(!http ? "Error No XMLHTTP available" : "Error: Empty URL");
+      return;       
     }
     
-    // Private
-    function getXMLHTTPObject() {  
-        var xmlHttp = false;
-        var AjaxClients = [
-            function () {return new ActiveXObject("Microsoft.XMLHTTP")},
-            function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-            function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-            function () {return new XMLHttpRequest()}
-        ];
-        
-        do {
-            try {
-                xmlHttp = AjaxClients.pop()();
-            } catch (e) {
-                continue;
-            }
-        } while (!xmlHttp && AjaxClients.length != 0)            
-        
-        return xmlHttp;
-    }  
-        
-    function dooSelect(element) {
-        var eltype   = element.substring(0, 1);
-        var _element = element.substring(1);
+    http.open((postQuery) ? "POST" : "GET", url, true);
     
-        return eltype != '.'
-                ? eltype != '#'
-                    ? document.getElementsByName(element)
-                    : document.getElementById(_element)
-                : document.getElementsByClassName(_element);
-      }  
-}
+    if(postQuery) {
+      http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    }
+    
+    http.onreadystatechange = function() {
+      if(http.readyState == 4) {
+        callback(http.responseText);
+      }
+    }
+    
+    http.send(postQuery);
+  }
+  
+  // Private
+  function getXMLHTTPObject() {
+    var xmlHttp = false;
+    var AjaxClients = [
+        function() { return new ActiveXObject("Microsoft.XMLHTTP") },
+        function() { return new ActiveXObject("Msxml3.XMLHTTP") },
+        function() { return new ActiveXObject("Msxml2.XMLHTTP") },
+        function() { return new XMLHttpRequest() }
+    ];
+                       
+    while (!xmlHttp && AjaxClients.length != 0) {
+      try {
+        xmlHttp = AjaxClients.pop()();
+      } catch (e) {
+        continue;
+      }
+    } 
+    return xmlHttp;
+  }
+  
+  // Private   
+  function dooSelect(element) {
+    var eltype   = element.substring(0, 1);
+    var _element = element.substring(1);
+  
+    return eltype != '#' 
+          ? eltype == '.'
+              ? document.getElementsByClassName(_element) // .className     
+              : document.getElementsByName(element)       // elemntName
+          : document.getElementById(_element);            // #elementId   
+  }
+  
+  return {
+       validateCaptcha: validateCaptcha,
+       refreshCapcha: refreshCapcha,
+       doFormSubmit: doFormSubmit
+  }
+    
+}()); 
 
 var fire = function(f) {
   (/load/.test(document.readyState)) ? setTimeout('fire(' + f + ')', 10) : f();
